6e4605ea68c77648f26fcbecadaa2dfdff4e4bca
Andy Teucher <andy.teucher@gmail.com>
2025-01-28 12:14:02 -0800

    Add cost summary start

---
+++ R/cost-summaries.R
@@ -0,0 +1,45 @@
+#' Get daily AWS usage costs
+#'
+#' @param end_date A Date object representing the end of the desired date range.
+#' @param months_back Optional. A single integer specifying how many months back to query.
+#'
+#' @returns
+#' A data frame of AWS usage costs.
+#'
+#' @export
+get_daily_usage_costs <- function(
+  end_date = Sys.Date(),
+  months_back = 6,
+  cost_type = c("unblended", "blended", "all")
+) {
+  end_date <- check_valid_date(end_date)
+
+  if (!rlang::is_integerish(months_back) || months_back > 12) {
+    cli::cli_abort("{.arg months_back} must be an integer <= 12.")
+  }
+
+  cost_type <- match.arg(cost_type)
+
+  start_date <- lubridate::floor_date(
+    lubridate::add_with_rollback(end_date, -months(months_back)),
+    unit = "month"
+  )
+
+  raw_daily <- sixtyfour::aws_billing(
+    as.character(start_date),
+    as.character(end_date),
+    filter = list(
+      Dimensions = list(
+        Key = "RECORD_TYPE",
+        Values = "Usage"
+      )
+    )
+  )
+
+  if (cost_type != "all") {
+    raw_daily <- dplyr::filter(raw_daily, id == cost_type)
+  }
+
+  raw_daily |>
+    dplyr::mutate(date = lubridate::ymd(.data$date))
+}
--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,30 @@ unsanitize_dir_names <- function(x) {
   x <- gsub("-5f", "_", x)
   x
 }
+
+check_valid_date <- function(
+  x,
+  arg = rlang::caller_arg(x),
+  call = rlang::caller_env()
+) {
+  if (inherits(x, "Date") && length(x) == 1L) {
+    return(x)
+  }
+
+  date <- try(as.Date(x), silent = TRUE)
+
+  if (
+    inherits(date, "try-error") ||
+      length(date) > 1 ||
+      all(is.na(date)) ||
+      is.null(date)
+  ) {
+    cli::cli_abort(
+      "{.arg {arg}} must be a length 1 Date or POSIXt object, or character in a standard unambiguous date format",
+      arg = arg,
+      call = call
+    )
+  }
+
+  date
+}
