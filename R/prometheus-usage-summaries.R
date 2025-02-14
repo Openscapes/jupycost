99c497fc00130c5b0f6ba6dc9cb9af15245b2d3e
Andy Teucher <andy.teucher@gmail.com>
2025-02-13 21:00:45 -0800

    expose grafana_url and grafana_token in summaries

--- R/prometheus-usage-summaries.R
+++ R/prometheus-usage-summaries.R
@@ -4,18 +4,23 @@
 #' @param step Time step in days (default `1`).
 #' @param aggregation time period over which to aggregate, in days (integer,
 #'   default `1`).
+#' @inheritParams query_prometheus_range
 #'
 #' @returns
 #' A data frame with daily user counts, grouped by namespace.
 #'
 #' @export
 get_daily_users <- function(
+  grafana_url = "https://grafana.openscapes.2i2c.cloud",
+  grafana_token = Sys.getenv("GRAFANA_TOKEN"),
   start_time = end_time - 30,
   end_time = Sys.Date(),
   aggregation = 1,
   step = 1
 ) {
   res <- query_prometheus_range(
+    grafana_url = grafana_url,
+    grafana_token = grafana_token,
     query = glue::glue(
       'count(
         sum(
@@ -38,8 +43,8 @@ get_daily_users <- function(
     dplyr::mutate(date = as.Date(date)) |>
     # Fill in zeros for missing dates
     tidyr::complete(
-      date = tidyr::full_seq(date, 1),
-      namespace,
+      date = tidyr::full_seq(.data$date, 1),
+      .data$namespace,
       fill = list(n_users = 0)
     )
 }
@@ -55,16 +60,20 @@ get_daily_users <- function(
 #'
 #' @export
 get_hourly_users <- function(
+  grafana_url = "https://grafana.openscapes.2i2c.cloud",
+  grafana_token = Sys.getenv("GRAFANA_TOKEN"),
   start_time = end_time - 30,
   end_time = Sys.Date(),
   step = "1h0m0s"
 ) {
   res <- query_prometheus_range(
+    grafana_url = grafana_url,
+    grafana_token = grafana_token,
     query = 'sum(
-  kube_pod_status_phase{phase="Running"}
-  * on(pod, namespace) 
-  kube_pod_labels{label_app="jupyterhub", label_component="singleuser-server"}
-) by (namespace)',
+        kube_pod_status_phase{phase="Running"}
+        * on(pod, namespace) 
+        kube_pod_labels{label_app="jupyterhub", label_component="singleuser-server"}
+      ) by (namespace)',
     start_time = start_time,
     end_time = end_time,
     step = step
