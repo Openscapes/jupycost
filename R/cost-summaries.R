bb4ab26cb456d8209ad311674b190d5403458b3f
Andy Teucher <andy.teucher@gmail.com>
2025-04-21 18:30:49 -0700

    Add ability to filter hub by tag in ce

--- R/cost-summaries.R
+++ R/cost-summaries.R
@@ -11,9 +11,12 @@
 get_daily_usage_costs <- function(
   end_date = Sys.Date(),
   months_back = 6,
-  cost_type = c("unblended", "blended", "all")
+  cost_type = c("unblended", "blended", "all"),
+  hub = c("all", "prod", "staging", "workshop")
 ) {
   end_date <- check_valid_date(end_date)
+
+  hub <- match.arg(hub)
 
   if (!rlang::is_integerish(months_back) || months_back > 12) {
     cli::cli_abort("{.arg months_back} must be an integer <= 12.")
@@ -26,15 +29,44 @@ get_daily_usage_costs <- function(
     unit = "month"
   )
 
+  filter_list <- list(
+    Dimensions = list(
+      Key = "RECORD_TYPE",
+      Values = list("Usage")
+    )
+  )
+
+  if (hub == "shared") {
+    filter_list = list(
+      And = list(
+        filter_list,
+        list(
+          Tags = list(
+            Key = "2i2c:hub-name",
+            MatchOptions = list("ABSENT")
+          )
+        )
+      )
+    )
+  } else if (hub != "all") {
+    filter_list = list(
+      And = list(
+        filter_list,
+        list(
+          Tags = list(
+            Key = "2i2c:hub-name",
+            Values = list(hub),
+            MatchOptions = list("EQUALS")
+          )
+        )
+      )
+    )
+  }
+
   raw_daily <- sixtyfour::aws_billing(
     as.character(start_date),
     as.character(end_date),
-    filter = list(
-      Dimensions = list(
-        Key = "RECORD_TYPE",
-        Values = "Usage"
-      )
-    )
+    filter = filter_list
   )
 
   if (cost_type != "all") {
